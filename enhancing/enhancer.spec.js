27083212c415566c8a89a94d4410a3f8fd895c72
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-04-08 14:19:57 -0400

    npm init -y. yarn add jest --dev. Wrote first test for repair(item).

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,8 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+it('should take in an item and return a new item with durability === 100', () => {
+  expect(
+    repair({ name: 'Sword of Regret', enhancement: 12, durability: 28 })
+  ).toBe({ name: 'Sword of Regret', enhancement: 12, durability: 100 });
+});
