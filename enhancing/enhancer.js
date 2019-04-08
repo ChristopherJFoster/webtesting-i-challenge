670ec02ef0419e46b54707880ee3e42ed60fad47
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-04-08 14:38:16 -0400

    Wrote test and function for succeed(item). Refactored test to use consts received and expected.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,7 +6,8 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  const enhancement = item.enhancement === 20 ? 20 : item.enhancement + 1;
+  return { ...item, enhancement };
 }
 
 function fail(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -2,11 +2,29 @@ const enhancer = require('./enhancer.js');
 // test away!
 
 it('should take in an item and return a new item with durability === 100', () => {
-  expect(
-    enhancer.repair({
-      name: 'Sword of Regret',
-      enhancement: 12,
-      durability: 28
-    })
-  ).toEqual({ name: 'Sword of Regret', enhancement: 12, durability: 100 });
+  const received = {
+    name: 'Sword of Regret',
+    enhancement: 12,
+    durability: 28
+  };
+  const expected = {
+    name: 'Sword of Regret',
+    enhancement: 12,
+    durability: 100
+  };
+  expect(enhancer.repair(received)).toEqual(expected);
+});
+
+it('should take in an item and return a new item with enhancement + 1 (up to a max of 20)', () => {
+  const received = {
+    name: 'Sword of Regret',
+    enhancement: 12,
+    durability: 28
+  };
+  const expected = {
+    name: 'Sword of Regret',
+    enhancement: 13,
+    durability: 28
+  };
+  expect(enhancer.succeed(received)).toEqual(expected);
 });
